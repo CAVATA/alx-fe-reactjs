2b6ca99f9e538d9ed400e5d5fdef59dd5f9d2be8
Jane Kyalo <cavatabernard@gmail.com>
2025-09-07 10:38:05 +0300

    Fix App.jsx and integrate WelcomeMessage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+
 
 function App() {
- return (
+  return (
     <>
-      <WelcomeMessage/>
+      
+       <WelcomeMessage /> 
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
