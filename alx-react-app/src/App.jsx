91a0e5f2c5411f12adc13ef652fe2dcbc8ca3772
Jane Kyalo <cavatabernard@gmail.com>
2025-09-07 19:17:02 +0300

    feat: add Header, MainContent, Footer components and assemble in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,14 @@
 
-
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import WelcomeMessage from './components/WelcomeMessage';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <WelcomeMessage />
       <MainContent />
       <Footer />
     </div>
